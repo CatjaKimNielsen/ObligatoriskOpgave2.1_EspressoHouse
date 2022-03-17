5b07bc7dd9556d0614e9a396213526c11d134125
catj0073@edu.zealand.dk <ZeaZEYVH!2>
2022-03-17 23:44:37 +0100

    "På menuen" er kodet færdig

--- PåMenuen.php
+++ PåMenuen.php
@@ -32,97 +32,158 @@
 
     <?php include "includes/navigation.php"; ?>
 
+    <div class="container-fluid w-100">
+        <img src="image/PåMenuen_KaffeKage.jpg" class="w-100 mb-4" alt="Lækker hjemmebag med kaffe og smoothie">
+    </div>
+
     <div class="container">
 
-        <h3 class="ms-1 mt-3 me-1 mb-4 text-center"> Klik gennem Espresso House's lækre menu og find dine favoritter. <h2>
+        <h3 class="ms-1 mt-3 me-1 mb-4 text-center"> Klik gennem Espresso House's lækre menu og find dine favoritter. </h3>
 
-        <img src="image/PåMenuen_KaffeCrossiant.jpg" class="w-100 mb-4" alt="Lækker hjemmebag med kaffe og smoothie">
+        <article class="ms-5 mt-4 me-5 mb-5">
 
-        <div class="collapse1_VarmeDrikke mb-3">
-            <div>
-                <p>
-                    <a class="btn btn-light_green w-100 dropdown-toggle" data-bs-toggle="collapse" href="#collapse1" role="button" aria-expanded="false" aria-controls="collapseExample">
-                        Varme drikke
-                    </a>
-                </p>
-                <div class="collapse" id="collapse1">
-                    <div class="card card-body">
-                        <div class="card" style="width: 14rem;">
-                            <img src="image/Menu_VarmDrik.jpg" class="card-img-top" alt="Lækker Americano i kaffekop">
-                            <div class="card-body">
-                                <p class="card-title">Americano</p>
+            <p class="mb-3">For os betyder kaffe rigtig meget, men det er ikke alt - vores hjerter brænder også for
+            andre varme drikke, kolde drikke, bagværk, sandwiches og salater. Fælles for alt, vi servere,
+            er den højde kvalitet på alle vores produkter.</p>
+
+            <div class="collapse1_VarmeDrikke mb-3">
+
+                <div>
+                    <p>
+                        <a class="btn btn-light_green w-100 dropdown-toggle" data-bs-toggle="collapse" href="#collapse1" role="button" aria-expanded="false" aria-controls="collapseExample">
+                            Varme drikke
+                        </a>
+                    </p>
+                    <div class="collapse" id="collapse1">
+                        <div class="card card-body">
+                            <div class="card" style="width: 14rem;">
+                                <img src="image/Menu_VarmDrik.jpg" class="card-img-top" alt="Lækker Americano i kaffekop">
+                                <div class="card-body">
+                                    <p class="card-text w-100 d-flex justify-content-center" style="font-weight: bold">Americano</p>
+                                    <p class="card-text w-100 d-flex justify-content-center">Standard: 36,00 kr.</p>
+                                </div>
                             </div>
                         </div>
-                        <div class="card" style="width: 14rem;">
-                            <img src="image/Menu_VarmDrik.jpg" class="card-img-top" alt="Lækker Caffe Latte i kaffekop">
-                            <div class="card-body">
-                                <p class="card-title">Caffe Latte</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="collapse2_KoldeDrikke mb-3">
+                <div>
+                    <p>
+                        <a class="btn btn-light_green w-100 dropdown-toggle" data-bs-toggle="collapse" href="#collapse2" role="button" aria-expanded="false" aria-controls="collapseExample">
+                            Kolde drikke
+                        </a>
+                    </p>
+                    <div class="collapse" id="collapse2">
+                        <div class="card card-body">
+                            <div class="card" style="width: 14rem;">
+                                <img src="image/Menu_KoldDrik.png" class="card-img-top" alt="Lækker Frapino Cream Chokolade">
+                                <div class="card-body">
+                                    <p class="card-text w-100 d-flex justify-content-center" style="font-weight: bold">Frapino Chokolade</p>
+                                    <p class="card-text w-100 d-flex justify-content-center">Standard: 56,00 kr.</p>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
 
-        <div class="collapse2_KoldeDrikke mb-3">
-            <div>
-                <p>
-                    <a class="btn btn-light_green w-100 dropdown-toggle" data-bs-toggle="collapse" href="#collapse2" role="button" aria-expanded="false" aria-controls="collapseExample">
-                        Kolde drikke
-                    </a>
-                </p>
-                <div class="collapse" id="collapse2">
-                    <div class="card card-body">
-                        ...
+            <div class="collapse3_Sandwiches mb-3">
+                <div>
+                    <p>
+                        <a class="btn btn-light_green w-100 dropdown-toggle" data-bs-toggle="collapse" href="#collapse3" role="button" aria-expanded="false" aria-controls="collapseExample">
+                            Sandwiches
+                        </a>
+                    </p>
+                    <div class="collapse" id="collapse3">
+                        <div class="card card-body">
+                            <div class="card" style="width: 14rem;">
+                                <img src="image/Menu_Sandwich.png" class="card-img-top" alt="Lækker foccacia med kylling og grillet peberfrugt">
+                                <div class="card-body">
+                                    <p class="card-text w-100 d-flex justify-content-center" style="font-weight: bold">Foccacia med kylling</p>
+                                    <p class="card-text w-100 d-flex justify-content-center">59,00 kr.</p>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
 
-        <div class="collapse3_Sandwiches mb-3">
-            <div>
-                <p>
-                    <a class="btn btn-light_green w-100 dropdown-toggle" data-bs-toggle="collapse" href="#collapse3" role="button" aria-expanded="false" aria-controls="collapseExample">
-                        Sandwiches
-                    </a>
-                </p>
-                <div class="collapse" id="collapse3">
-                    <div class="card card-body">
-                        ...
+            <div class="collapse4_Salater mb-3">
+                <div>
+                    <p>
+                        <a class="btn btn-light_green w-100 dropdown-toggle" data-bs-toggle="collapse" href="#collapse4" role="button" aria-expanded="false" aria-controls="collapseExample">
+                            Salater
+                        </a>
+                    </p>
+                    <div class="collapse" id="collapse4">
+                        <div class="card card-body">
+                            <div class="card" style="width: 14rem;">
+                                <img src="image/Menu_Salat.jpg" class="card-img-top" alt="Lækker salat med quinoa og hummus">
+                                <div class="card-body">
+                                    <p class="card-text w-100 d-flex justify-content-center" style="font-weight: bold">Salat med quinoa</p>
+                                    <p class="card-text w-100 d-flex justify-content-center">45,00 kr.</p>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
 
-        <div class="collapse4_Salater mb-3">
-            <div>
-                <p>
-                    <a class="btn btn-light_green w-100 dropdown-toggle" data-bs-toggle="collapse" href="#collapse4" role="button" aria-expanded="false" aria-controls="collapseExample">
-                        Salater
-                    </a>
-                </p>
-                <div class="collapse" id="collapse4">
-                    <div class="card card-body">
-                        ...
+            <div class="collapse5_Bagværk mb-3">
+                <div>
+                    <p>
+                        <a class="btn btn-light_green w-100 dropdown-toggle" data-bs-toggle="collapse" href="#collapse5" role="button" aria-expanded="false" aria-controls="collapseExample">
+                            Hjemmelavet bagværk
+                        </a>
+                    </p>
+                    <div class="collapse" id="collapse5">
+                        <div class="card card-body">
+                            <div class="card" style="width: 14rem;">
+                                <img src="image/Menu_Bagværk.png" class="card-img-top" alt="Lækker chokolademuffin">
+                                <div class="card-body">
+                                    <p class="card-text w-100 d-flex justify-content-center" style="font-weight: bold">Chokolademuffin</p>
+                                    <p class="card-text w-100 d-flex justify-content-center">29,00 kr.</p>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
 
-        <div class="collapse5_Bagværk mb-5">
-            <div>
-                <p>
-                    <a class="btn btn-light_green w-100 dropdown-toggle" data-bs-toggle="collapse" href="#collapse5" role="button" aria-expanded="false" aria-controls="collapseExample">
-                        Hjemmelavet bagværk
-                    </a>
-                </p>
-                <div class="collapse" id="collapse5">
-                    <div class="card card-body">
-                        ...
+            <div class="collapse6_KaffeDerhjemme mb-5">
+                <div>
+                    <p>
+                        <a class="btn btn-light_green w-100 dropdown-toggle" data-bs-toggle="collapse" href="#collapse6" role="button" aria-expanded="false" aria-controls="collapseExample">
+                            Kaffe derhjemme
+                        </a>
+                    </p>
+                    <div class="collapse" id="collapse6">
+                        <div class="mt-2">
+                            <h6 class="mb-2"> Nyd kaffe fra Espresso House derhjemme </h6>
+                            <p class="mb-4"> Hos Espresso House er kaffe hjertet i alt vi gør. I alle vores kaffebarer
+                            ønsker vi at skabe en pause og en tid til hygge og samvær. Og det samme
+                            ønsker vi, at du skal kunne gøre derhjemme. <br><br> Vi har søgt, sorteret
+                            og smagt utallige kaffer for at sammensætte forskellige smagfulde blends,
+                            så du kan finde din egen personlige favorit – og brygge præcis den samme
+                            kop kaffe derhjemme, som du får i vores kaffebarer.</p>
+                            <p>Vi kan tilbyde følgende kaffer derhjemme:
+                            <br> - Signatur Brew
+                            <br> - Organic Brew
+                            <br> - Dark Rost
+                            <br> - Signature Espresso
+                            <br> - Organic Espresso
+                            <br> - Bold Grace
+                            <br> - Dashing Dark
+                            </p> <br>
+                            <img src="image/PåMenuen_Kaffeposer.jpg" alt="Frisklavet kaffe, Espresso House kaffeposer og kapsler">
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+
+        </article>
 
     </div>
 
