127923b1941da5ee4cb4683e4493854919baed69
catj0073@edu.zealand.dk <ZeaZEYVH!2>
2022-03-17 11:52:38 +0100

    Kontakt-undersiden er kodet færdig

--- Kontakt.php
+++ Kontakt.php
@@ -32,7 +32,35 @@
 
     <?php include "includes/navigation.php"; ?>
 
-    <div> KONTAKTFORMULAR + HENVISNING TIL FAQ + KONTAKTMULIGHEDER SOM FX MAILADRESSE, TELEFONNUMRE MV. </div>
+    <div class="container-fluid w-100">
+        <img src="image/Kontakt_EH.jpg" alt="Hyggelig stemning i Espresso House Café">
+    </div>
+
+    <article class="container">
+        <h3 class="ms-1 mt-3 me-1 mb-3 text-center">Kontaktformular</h3>
+        <p class="mb-3 text-center">Vi bestræber os på den bedst mulige kundeservice og du vil derfor få svar på din
+        henvendelse inden for 24 timer.</p>
+        <form class="mb-5">
+            <div class="mb-3">
+                <label for="navn" class="form-label">Dit fulde navn</label>
+                <input type="text" class="form-control" id="navn">
+            </div>
+            <div class="mb-3">
+                <label for="email" class="form-label">Din e-mail</label>
+                <input type="email" class="form-control" id="email">
+            </div>
+            <div class="mb-3">
+                <label for="besked" class="form-label">Din besked</label>
+                <input type="text" class="form-control " id="besked" aria-describedby="email_tekst" style="height: 200px">
+                <div id="email_tekst" class="form-text">Bare rolig, vi deler ikke dine oplysninger med nogen.</div>
+            </div>
+            <div class="mb-3 form-check">
+                <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                <label class="form-check-label" for="exampleCheck1" >Tjek om alt er udfyldt som det skal</label>
+            </div>
+            <button type="submit" class="btn btn-light_green w-25">Send besked</button>
+        </form>
+    </article>
 
     <?php include "includes/footer.php"; ?>
 
