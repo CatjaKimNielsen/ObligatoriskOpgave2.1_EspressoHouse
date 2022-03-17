c375d18f81cfb64f88e55e43f6d1554362f6e892
catj0073@edu.zealand.dk <ZeaZEYVH!2>
2022-03-17 10:24:25 +0100

    Denne underside er kodet færdig

--- VoresKaffe.php
+++ VoresKaffe.php
@@ -32,7 +32,94 @@
 
     <?php include "includes/navigation.php"; ?>
 
-    <div> INDSÆT NOGET LÆKKERT OM DERES KAFFE. BENYT EVT. DROPDOWNS OG KARRUSELLER! </div>
+    <div class="container-fluid w-100">
+        <img src="image/VoresKaffe_Kaffebønner.png">
+    </div>
+
+
+    <div class="container">
+
+        <h3 class="ms-1 mt-3 me-1 mb-4 text-center"> Kaffe i verdensklasse </h3>
+
+        <article class="ms-5 mt-4 me-5 mb-5">
+
+            <p> På Espresso House brænder vi for en fantastisk kaffeoplevelse. Alt vores kaffe er
+            specialkaffe og det betyder, at kaffen altid når over 80 points på en kvalitetsskala
+            fra 0–100. Det er der kun 5 % af alt kaffe i verden, der gør. Med andre ord kan vi
+            bogstaveligt talt sige, at vores kaffe er i verdensklasse. <br><br> Hos os er
+            friskheden af kaffen lige så vigtig som selve oplevelsen. Friskristet kaffe kan udfolde
+            sin fulde smagskarakter - de mest delikate smagsnuancer og aromaer. For at sikre
+            dokumentationen af friskheden på vores kaffe, markerer vi alle poser med
+            ristningsdato- og tidspunkt.</p>
+
+            <div class="collapse1 mt-4 mb-3">
+
+                <div class="mb-3">
+                    <p>
+                        <a class="btn btn-light_green w-100 dropdown-toggle" data-bs-toggle="collapse" href="#collapse1" role="button" aria-expanded="false" aria-controls="collapseExample">
+                            Vores kafferisteri i Sverige
+                        </a>
+                    </p>
+                    <div class="collapse" id="collapse1">
+                        <div class="mt-2">
+                            <h6 class="mb-2"> Risteriet i Länna syg for Stockholm </h6>
+                            <p class="mb-4">I Länna syd for Stockholm har vi længe brygget på noget
+                            stort. Her har vi nemlig bygget vores helt eget kafferisteri. Og det er
+                            ikke bare hvilket som helst risteri. Det er Europas største og mest
+                            moderne specialkafferisteri! Nu rister vi altså selv alt vores kaffe
+                            til alle vores coffee shops. Det gør vi af flere grunde. Men først
+                            og fremmest, fordi vi elsker kaffe. Kaffe er ikke bare en drik. For os
+                            er den en kunst og et håndværk, som fortjener den allerbedste behandling.</p>
+                            <h6 class="mb-2"> Alt kaffe fortjener noget unikt </h6>
+                            <p class="mb-4"> Hver lot af kaffe der kommer til vores risteri har sin
+                            egen historie og karakter. Og den fortjener hver sin unikke ristning. Vi
+                            rister derfor vores kaffe i hånden, i små mængder med forskellige
+                            temperaturer og tider, så det passer bedst til lige præcis den kaffe,
+                            vi har fået fra kaffebønderne. Vigtigst af alt har vi god tid til at
+                            riste vores kaffe i lige præcis det tempo, som verdens bedste kaffe behøver.
+                            Det tempo er langsomt. Rigtigt langsomt. Metoden hedder også slowroasting,
+                            og med den fremkalder vi de mange fine smagsnuancer i kaffebønnerne. Hele
+                            risteprocessen bliver endda overværet og kontrolleret af vores egen
+                            Roastmaster, som er ekspert i at bringe det bedste frem i lige netop din
+                            kaffe. Og behøver vi nævne, at kaffen er endnu mere frisk, når vi selv
+                            står for ristningen og sender den direkte ud til vores coffee shops? </p>
+                            <img src="image/VoresKaffe_BønnerHænder.png">
+                        </div>
+                    </div>
+                </div>
+
+                <div class="mb-5">
+                    <p>
+                        <a class="btn btn-light_green w-100 dropdown-toggle" data-bs-toggle="collapse" href="#collapse2" role="button" aria-expanded="false" aria-controls="collapseExample">
+                            Vores kaffebønder
+                        </a>
+                    </p>
+                    <div class="collapse" id="collapse2">
+                        <div class="mt-2">
+                            <p class="mb-4">Hos Espresso House ønsker vi at tage ansvar fra bønne til
+                            kop for at sikre, at vi er en del af en bæredygtig kaffeproduktion. De
+                            første skridt er at skabe og opretholde et godt forhold til vores
+                            kaffebønder, at vide hvor kaffen har vokset, og under hvilke forhold.
+                            Vi mener, at tætte langsigtede relationer og samarbejde er vejen til
+                            gennemsigtighed og bæredygtighed i kaffeproduktionen.</p>
+                            <h6 class="mb-2"> Bæredygtigt indkøb </h6>
+                            <p class="mb-4">Ud over vores adfærdskodeks for leverandører vejledes
+                            vi af vores interne program når kaffebønnerne skal indkøbes. Den
+                            indeholder rutiner, tiltag og principper, som vi følger for at sikre et
+                            bæredygtigt indkøb. Programmet inkluderer interne besøg og vurderinger
+                            af vore kaffebønder samt eksterne tredjepartsvurderinger foretaget af
+                            landmænd og samarbejdspartnere. Arbejdet koordineres i et internt råd
+                            med medlemmer fra både Espresso House og Johan & Nyström.</p>
+                            <img src="image/VoresKaffe_Kaffebønder.jpg">
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+        </article>
+
+    </div>
 
     <?php include "includes/footer.php"; ?>
 
